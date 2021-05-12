245f1d0fc4b9a5441b2d99e4a20b04afdd9aba60
Jesper Frijns <jesperfrijns@hotmail.com>
2021-05-12 08:15:54 +0200

    Added to {user_data dictionary, started on automating the element calling function

--- project/process.py
+++ project/process.py
@@ -4,47 +4,58 @@
 title: process.py
 project: Link-Budget-Toolbox
 date: 09/05/2021
-author: lmaio
+author: Jesper Frijns
 """
 
 # Two ways of importing the link element classes
 import project.link_element as le
 
 #TODO: Import dictionary to GUI
-user_data = {'setting' : 'nominal',
+
+#TODO: Put right link element names in 'elements' to call
+
+# This is a temporary dictionary. In the end the dictionary should be filled in automatically based on user input.
+user_data = {'setting' : {'case_type' : 'nominal'},
              'generic_values' : {'altitude':800},
-             'elements' : {'spacecraft_transmitter' : {'link_type' :    'TRANSMITTER',
+             'elements' : {'SC_TX_Ant' :              {'link_type'  :   'TRANSMITTER',
                                                        'input_type' :   'gainloss',
+                                                       'gain_loss'  :    10,
                                                        'parameters' :  {'parameter1' : None,
                                                                         'parameter2' : None,
                                                                         'parameter3' : None}},
-                           'free_space' :              {'link_type' :   'FREE_SPACE',
+                           'free_space' :             {'link_type' :    'FREE_SPACE',
                                                        'input_type' :   'gainloss',
+                                                       'gain_loss' :     -8,
                                                        'parameters' :  {'parameter1' : None,
                                                                         'parameter2' : None,
                                                                         'parameter3' : None}},
-                           'ground_station_receiver': {'link_type':     'RECEIVER',
-                                                       'input_type':    'gainloss',
-                                                       'parameters':   {'parameter1': None,
-                                                                       'parameter2': None,
-                                                                       'parameter3': None}}
+                           'GS_RX_Ant':               {'link_type'  :   'RECEIVER',
+                                                       'input_type' :   'gainloss',
+                                                       'gain_loss'  :    2,
+                                                       'parameters' :  {'parameter1' : None,
+                                                                        'parameter2' : None,
+                                                                        'parameter3' : None}}
                            }
              }
 
 
 
 
-
-#TODO: Create dictionary
-#TODO: Read out dictionary and check for linktypes
-#TODO: Based on linktypes, call link elements
-#TODO: Get gainloss from link elements
-#TODO: Sum link elements
-#TODO: Decide on export file (JSON, YAML, etc.)
-#TODO: Save to file for GUI
-
-
 if __name__ == '__main__':
+    # TODO: Read out dictionary and check for linktypes
+    # TODO: Based on linktypes, call link elements
+    # TODO: Get gainloss from link elements
+    for i in user_data['elements']:
+        # Access seperate link elements one by one, check for input_type and call with correct variables each link
+        # element file.
+        print(i, user_data['elements'])
+        #TODO: Make sure that all link element classes have same naming convention for callability's sake
+        #TODO: Make sure that all link element classes are in a seperate file
+
+    # TODO: Sum link elements
+    # TODO: Decide on export file (JSON, YAML, etc.)
+    # TODO: Save to file for GUI
+
     generic = le.LinkElement('Example 1', 'GENERIC', 3)
 
     eirp_example = le.EIRPElement('EIRP Example', parameters=[5, 23])
