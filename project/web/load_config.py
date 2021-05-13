af289a414b34bfa47ff87335cce2dd3babf5af1a
Luigi Maiorano <lmaiorano97@gmail.com>
2021-05-13 23:51:39 +0200

    Rough sketch of pyqt app

--- project/web/load_config.py
+++ project/web/load_config.py
@@ -8,11 +8,25 @@ author: lmaio
 """
 
 from browser import document, window, console
+import yaml
+import json
+
+def load_data():
+    storage = window.sessionStorage
+
+    data = storage.get("config")
+    if data:
+        return json.loads(data)
+    else:
+        storage["config"] = json.dumps({})
+        return {}
 
 
 
 def file_read(e):
+    storage = window.sessionStorage
     def onload(e):
+
         try:
             # Print contents in textarea
             document['config-text'].value = e.target.result
@@ -20,8 +34,9 @@ def file_read(e):
             console.log(f'Cannot show file contents, textarea "{KE}" does not exist')
 
         # save config to session storage
+        data = yaml.safe_load(e.target.result)
+        storage.setItem('config', data)
 
-    storage = window.sessionStorage
 
     file = document['file-loadconfig'].files[0]
     reader = window.FileReader.new()
