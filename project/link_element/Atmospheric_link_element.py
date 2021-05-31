6022bc75a039e3bcc0a12005a72d9c692942f3d2
Luigi Maiorano <l.h.maiorano@student.tudelft.nl>
2021-05-31 11:23:44 +0200

    restored atmospheric changes

--- project/link_element/Atmospheric_link_element.py
+++ project/link_element/Atmospheric_link_element.py
@@ -6,14 +6,15 @@ Created on Sun May 23 18:46:47 2021
 """
 from project.link_element import LinkElement
 import numpy as np
-# TODO: Populate this element with either the equation given in space instrumentation course or the tables from intelsat in microsat course
+
+c = 299792458   #[m/s]
 
 class Atmospheric_LinkElement(LinkElement):
     '''Specific type of LinkElement for the Atmospheric loss,
     that can depend a single gain/loss value or on parameters instead. as
     dictated by the input_type value
 
-    Although not defined here, methods "get_gain()" and "get_loss()" are 
+    Although not defined here, methods "get_gain()" and "get_loss()" are
     automatically inherited and will also work
     '''
     def __init__(self, name, input_type, gain, parameters):
@@ -22,9 +23,97 @@ class Atmospheric_LinkElement(LinkElement):
         # Add attributes that are unique to TxElement
         self.input_type = input_type
 
-        # self.efficiency = parameters.get('antenna_efficiency', None)
-        # self.diameter = parameters.get('antenna_diameter', None)
-        # self.wavelength = parameters.get('wavelength', None)
+        self.t = parameters.get('air_temperature', None)        # [degC]
+        self.p = parameters.get('air_pressure', None)           # [hPa]
+        self.ro = parameters.get('water_vapor_content', None)   # [g/m**3]
+        self.wavelength = parameters.get('wavelength', None)    # [m]
+        self.angle = parameters.get('elevation_angle', None)    # [deg]
+        self.f = c/self.wavelength
 
-        # if self.input_type != 'gain_loss':
-        #     self.process()
+        if self.input_type != 'gain_loss':
+            self.process()
+
+    def process(self):
+        self.gain = (self.attenuationWetAir() + self.attenuationDryAir()) / np.sin(self.angle / 180 * np.pi)
+    def attenuationDryAir(self):
+        # approximation valid up to 54 GHz
+        e = self.ro* (self.t + 273.15) / 216.7
+        ptot= self.p + e
+        rp= ptot/ 1013
+        rt= 288 / (273 + self.t)
+
+        xi1 = self.phi(rp, rt, 0.0717, -1.8132, 0.0156, -1.6515)
+        xi2 = self.phi(rp, rt, 0.5146, -4.6368, -0.1921, -5.7416)
+        xi3 = self.phi(rp, rt, 0.3414, -6.5851, 0.2130, -8.5854)
+        print(rt)
+        print(rp)
+        print(self.f)
+        print(xi1)
+        print(xi2)
+        print(xi3)
+        # TODO: find out what is causing the RuntimeWarning: invlaid value encountered in double scalars
+        gamma0 = (((7.2 * rt**(2.8)) / (self.f**2 + 0.34 * rp**2 * rt**(1.6)))+ ((0.62 * xi3) / ((54 -self.f)**(1.16 * xi1) + 0.83 * xi2)))* self.f**2 * rp**2 * 1e-3
+
+        t1 = 4.64 / (1 + 0.066 * rp**(-2.3)) * np.exp(
+            -((self.f -59.7) / (2.87 + 12.4 * np.exp(-7.9 * rp)))**2)
+        t2 = 0.14 * np.exp(2.12 * rp) / ((self.f -118.75)**2 + 0.031 * np.exp(2.2 * rp))
+        t3 = 0.0114 / (1 + 0.14 * rp**(-2.6)) * self.f * (
+            -0.0247 + 0.0001 * self.f + 1.61e-6 * self.f**2) / (
+                1 -0.0169 * self.f + 4.1e-5 * self.f**2 + 3.2e-7 * self.f**3)
+        h0 = 6.1 / (1+0.17* rp**(1.1)) * (1+t1 +t2 + t3)
+        att = h0 * gamma0
+
+        return att
+
+    def attenuationWetAir(self):
+        # approximation valid up to 350 GHz
+        e = self.ro* (self.t + 273.15) / 216.7
+        ptot= self.p + e
+        rp= ptot/ 1013
+        rt= 288 / (273 + self.t)
+
+        eta1 = 0.955 * rp* rt**(0.68) + 0.006 * self.ro
+        eta2 = 0.735 * rp* rt**(0.5) + 0.0353 * rt**4 * self.ro
+
+        gamma1 = self.g(self.f, 22) * (3.98 * eta1 * np.exp(2.23 * (1 -rt))) / ((self.f -22.235)**2 + 9.42 * eta1**2) + (11.96 * eta1 * np.exp(0.7 * (1 -rt))) / ((self.f -183.31)**2 + 11.14 * eta1**2)
+        gamma2 = ((0.081 * eta1 * np.exp(6.44 * (1 -rt))) / ((self.f -321.226)**2 + 6.29 * eta1**2)) + ((3.66 * eta1 * np.exp(1.6 * (1 -rt))) / ((self.f -325.153)**2 + 9.22 * eta1**2))
+        gamma3 = ((25.37 * eta1 * np.exp(1.09 * (1 -rt))) / ((self.f -380)**2)) + ((17.4 * eta1 * np.exp(1.46 * (
+                1 -rt))) /((self.f -448)**2))
+        gamma4 = (self.g(self.f, 557) * (844.6 * eta1 * np.exp(0.17 * (1 -rt))) /(
+            (self.f -557)**2)) + (self.g(self.f, 752) * (290 * eta1 * np.exp(0.41 * (
+                1 -rt))) /((self.f -752)**2))
+        gamma5 = self.g(self.f, 1780) * (8.3328e4 * eta2 * np.exp(0.99 * (1 -rt))) /(
+            (self.f -1780)**2)
+        gammaw= (gamma1 + gamma2 + gamma3 + gamma4 + gamma5) * self.f**2 * rt**(
+            2.5) * self.ro* 1e-4
+
+        sigmaw= 1.013 / (1 + np.exp(-8.6 * (rp-0.57)))
+
+        t1 = (1.39 * sigmaw) / ((self.f -22.235)**2 + 2.56 * sigmaw)
+        t2 = (3.37 * sigmaw) / ((self.f -183.31)**2 + 4.69 * sigmaw)
+        t3 = (1.58 * sigmaw) / ((self.f -325.1)**2 + 2.89 * sigmaw)
+
+        hw= 1.66 * (1 + t1 + t2 + t3)
+
+        att = gammaw* hw
+
+        return att
+
+    def g(self,f,fi):
+        return 1 + ((f-fi) / (f+fi))**2
+
+    def phi(self,rp, rt, a, b, c, d):
+        return rp**a * rt**b * np.exp(c * (1 - rp) + d * (1 - rt))
+
+if __name__ == '__main__':
+    # Put any code here you want to use to test the class
+    # (like a scratch pad to test stuff while you're working)
+    testparameters = {'air_temperature': 15,
+                      'air_pressure': 1023,
+                      'water_vapor_content': 7.5,
+                      'wavelength': c/2e6,
+                      'elevation_angle': 89}
+    testelement = Atmospheric_LinkElement('test', 'parameter_set_2', -131, testparameters)
+    print(testelement)
+    testelement.process()
+    print(testelement)
