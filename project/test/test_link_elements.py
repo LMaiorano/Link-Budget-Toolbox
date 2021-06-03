d91fe45a3c4bb3016c156fd901bd5dcb48400811
Willem van Lynden <willemvl@live.com>
2021-06-03 12:37:32 +0200

    fixed the linkelement test class

--- project/test/test_link_elements.py
+++ project/test/test_link_elements.py
@@ -4,6 +4,7 @@ Created on Wed Jun  2 13:49:50 2021
 
 @author: Willem van Lynden
 """
+# TODO: test this with DELFT values as also validation
 import unittest
 from project.link_element import FREE_SPACE_LinkElement, RX_LinkElement, \
     TX_LinkElement, Atmospheric_LinkElement
@@ -12,7 +13,7 @@ Re = 6371e3     #[m]
 c = 299792458   #[m/s]
 
 class FREE_SPACE_LinkElementTest(unittest.TestCase):
-    def setup(self):
+    def test_outcome(self):
         testparameters = {'distance': 600e3,
                       'sc_altitude': 500e3,
                       'gs_altitude': 0,
@@ -21,9 +22,9 @@ class FREE_SPACE_LinkElementTest(unittest.TestCase):
 
         out_val = FREE_SPACE_LinkElement('test', 'parameter_set_2',0, testparameters).gain
         print(out_val)
-        ref_val = 103 # Should be -103
+        ref_val = -103 # Should be -103
 
-        self.assertAlmostEqual(ref_val, out_val,8)
+        self.assertAlmostEqual(ref_val, out_val,0)
 
 if __name__ == '__main__':
     unittest.main()
