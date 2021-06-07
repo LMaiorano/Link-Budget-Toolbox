48734b5c1bf520454a9459fb6aee70336f1f5268
Jesper Frijns <jesperfrijns@hotmail.com>
2021-06-07 09:49:00 +0200

    Fixed example_config.yaml, added tests in test_process, test_fill_results_data is still unfinished and incorrect

--- project/test/test_process.py
+++ project/test/test_process.py
@@ -12,6 +12,10 @@ class ProcessTestCase(unittest.TestCase):
         self.maxDiff = None
         self.data_bare_minimum = load_from_yaml('data/bare_minimum.yaml')
         self.data_generic_only = load_from_yaml('data/generic_only.yaml')
+        self.data_test_user_data = load_from_yaml('data/user_data.yaml')
+        self.ref_user_data = load_from_yaml('data/ref_user_data.yaml')
+        self.unit_converted_user_data = load_from_yaml('data/unit_converted_user_data.yaml')
+        self.ref_fill_results_data = load_from_yaml('data/ref_fill_results_data.yaml')
 
     def test_load_from_yaml(self):
         # Try to limit this to the only hard-coded dictionary. If this test passes, all other
@@ -34,116 +38,15 @@ class ProcessTestCase(unittest.TestCase):
         pass
 
     def test_read_user_data(self):
-        test_user_data = {'elements': {'Free Space': {'gain_loss': None,
-                                                 'idx': 3,
-                                                 'input_type': 'parameter_set_2',
-                                                 'link_type': 'FREE_SPACE',
-                                                 'parameters': {'angle': 10.0,
-                                                                'distance': 1500.0,
-                                                                'gs_altitude': 0.0,
-                                                                'sc_altitude': 300.0,
-                                                                'wavelength': 10.0}},
-                                  'GS RX Ant': {'gain_loss': None, 'idx': 1,
-                                                'input_type': 'parameter_set_1',
-                                                'link_type': 'RX',
-                                                'parameters': {'antenna_diameter': 1.0,
-                                                               'antenna_efficiency': 0.8,
-                                                               'wavelength': 100.0}},
-                                  'SC TX Ant': {'gain_loss': 10.0,
-                                                'idx': 2,
-                                                'input_type': 'gain_loss',
-                                                'link_type': 'GENERIC',
-                                                'parameters': None}},
-                     'general_values': {'input_power': 65,
-                                        'rx_sys_threshold': 6,
-                                        'total_gain': None,
-                                        'total_margin': None},
-                     'settings': {'case_type': 'nominal'}}
 
-        test_dict_user_data = {'name':
-                                   {0: 'Free Space',
-                                    1: 'GS RX Ant',
-                                    2: 'SC TX Ant'},
-                               'gain_loss': {0: None,
-                                             1: None,
-                                             2: 10.0},
-                               'idx': {0: 3,
-                                       1: 1,
-                                       2: 2},
-                               'input_type': {0: 'parameter_set_2',
-                                              1: 'parameter_set_1',
-                                              2: 'gain_loss'},
-                               'link_type': {0: 'FREE_SPACE',
-                                             1: 'RX',
-                                             2: 'GENERIC'},
-                               'parameters': {0: {'angle': 10.0,
-                                                  'distance': 1500.0,
-                                                  'gs_altitude': 0.0,
-                                                  'sc_altitude': 300.0,
-                                                  'wavelength': 10.0},
-                                              1: {'antenna_diameter': 1.0,
-                                                  'antenna_efficiency': 0.8,
-                                                  'wavelength': 100.0},
-                                              2: None}}
-
-        self.assertEqual(read_user_data(test_user_data).to_dict(), test_dict_user_data)
+        self.assertEqual(read_user_data(self.data_test_user_data).to_dict(), self.ref_user_data)
 
     def test_fill_results_data(self):
-        test_user_data = {'elements': {'Free Space': {'gain_loss': None,
-                                                      'idx': 3,
-                                                      'input_type': 'parameter_set_2',
-                                                      'link_type': 'FREE_SPACE',
-                                                      'parameters': {'angle': 10.0,
-                                                                     'distance': 1500.0,
-                                                                     'gs_altitude': 0.0,
-                                                                     'sc_altitude': 300.0,
-                                                                     'wavelength': 10.0}},
-                                       'GS RX Ant': {'gain_loss': None, 'idx': 1,
-                                                     'input_type': 'parameter_set_1',
-                                                     'link_type': 'RX',
-                                                     'parameters': {'antenna_diameter': 1.0,
-                                                                    'antenna_efficiency': 0.8,
-                                                                    'wavelength': 100.0}},
-                                       'SC TX Ant': {'gain_loss': 10.0,
-                                                     'idx': 2,
-                                                     'input_type': 'gain_loss',
-                                                     'link_type': 'GENERIC',
-                                                     'parameters': None}},
-                          'general_values': {'input_power': 65,
-                                             'rx_sys_threshold': 6,
-                                             'total_gain': None,
-                                             'total_margin': None},
-                          'settings': {'case_type': 'nominal'}}
 
-        test_df_user_data = read_user_data(test_user_data)
+        test_df_user_data = read_user_data(self.unit_converted_user_data)
+        out = fill_results_data(test_df_user_data, self.unit_converted_user_data)
 
-        test_result_data = {'elements': {'Free Space': {'gain_loss': -66.72664804890425,
-                                                        'idx': 3,
-                                                        'input_type': 'parameter_set_2',
-                                                        'link_type': 'FREE_SPACE',
-                                                        'parameters': {'angle': 10.0,
-                                                                       'distance': 1500.0,
-                                                                       'gs_altitude': 0.0,
-                                                                       'sc_altitude': 300.0,
-                                                                       'wavelength': 10.0}},
-                                         'GS RX Ant': {'gain_loss': -31.026102676197887,
-                                                       'idx': 1,
-                                                       'input_type': 'parameter_set_1',
-                                                       'link_type': 'RX',
-                                                       'parameters': {'antenna_diameter': 1.0,
-                                                                      'antenna_efficiency': 0.8,
-                                                                      'wavelength': 100.0}},
-                                         'SC TX Ant': {'gain_loss': 10.0,
-                                                       'idx': 2,
-                                                       'input_type': 'gain_loss',
-                                                       'link_type': 'GENERIC',
-                                                       'parameters': None}},
-                            'general_values': {'input_power': 65,
-                                               'rx_sys_threshold': 6,
-                                               'total_gain': None, 'total_margin': None},
-                            'settings': {'case_type': 'nominal'}}
-
-        self.assertEqual(fill_results_data(test_df_user_data, test_user_data), test_result_data)
+        self.assertEqual(out, self.ref_fill_results_data)
 
 
     def test_sum_results(self):
